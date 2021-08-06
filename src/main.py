0f3f710175150516430e5c222c0d87d03510655e
Hexye <65314629+HexyeDEV@users.noreply.github.com>
2021-08-07 01:56:15 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,3 +4,7 @@ import json
 def python_snippet():
   snippets = json.load(open("snip/snippets.json"))
   return random.choice(snippets["python_snip"])
+
+def php_snippet():
+  snippets = json.load(open("snip/snippets.json"))
+  return random.choice(snippets["php_snip"])
